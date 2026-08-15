50768b0e737a2db165935417f17812034ab7fc59
JovihanniCasenas <casenasjovihanni@gmail.com>
2026-08-15 12:56:50 +0800

    feat: add buy me a coffee link

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,9 +1,13 @@
 export default function Footer() {
     return (
-        <footer className="my-12 text-center text-sm text-gray-500">
-            <div>&copy; {new Date().getFullYear()} Bloomoji. All rights reserved.</div>
-            <div>Source code can be found on <a href="https://github.com/JovihanniCasenas/bloomoji" target="_blank" rel="noopener noreferrer" className="text-blue-900">GitHub</a>.</div>
-            <div>Created by <a href="https://portfolio.jovihanni.com" target="_blank" rel="noopener noreferrer" className="text-blue-900">Jovihanni Casenas</a>.</div>
+        <footer className="my-12 text-center">
+            <div className="mb-4 text-lg">
+                <p className="text-gray-500 mb-0">Enjoying Bloomoji?</p>
+                <a href="https://buymeacoffee.com/jovihanni" target="_blank" rel="noopener noreferrer">Buy me a coffee ☕</a>
+            </div>
+            <div className="text-sm text-gray-500">&copy; {new Date().getFullYear()} Bloomoji. All rights reserved.</div>
+            <div className="text-sm text-gray-500">Source code can be found on <a href="https://github.com/JovihanniCasenas/bloomoji" target="_blank" rel="noopener noreferrer" className="text-blue-900">GitHub</a>.</div>
+            <div className="text-sm text-gray-500">Created by <a href="https://portfolio.jovihanni.com" target="_blank" rel="noopener noreferrer" className="text-blue-900">Jovihanni Casenas</a>.</div>
         </footer>
     )
 }
