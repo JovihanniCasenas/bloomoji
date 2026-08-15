610c2f16f99e12998e05a1c1dc4e601c2f6af3a0
JovihanniCasenas <casenasjovihanni@gmail.com>
2026-08-15 12:58:28 +0800

    fix: hard code year

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -5,7 +5,7 @@ export default function Footer() {
                 <p className="text-gray-500 mb-0">Enjoying Bloomoji?</p>
                 <a href="https://buymeacoffee.com/jovihanni" target="_blank" rel="noopener noreferrer">Buy me a coffee ☕</a>
             </div>
-            <div className="text-sm text-gray-500">&copy; {new Date().getFullYear()} Bloomoji. All rights reserved.</div>
+            <div className="text-sm text-gray-500">&copy; 2026 Bloomoji. All rights reserved.</div>
             <div className="text-sm text-gray-500">Source code can be found on <a href="https://github.com/JovihanniCasenas/bloomoji" target="_blank" rel="noopener noreferrer" className="text-blue-900">GitHub</a>.</div>
             <div className="text-sm text-gray-500">Created by <a href="https://portfolio.jovihanni.com" target="_blank" rel="noopener noreferrer" className="text-blue-900">Jovihanni Casenas</a>.</div>
         </footer>
